caa8ea43ecfa387bf5cc24a13f2bd18ef6f771b4
alexander-mipt <antipov.ae@phystech.edu>
2022-11-02 17:50:19 +0300

    [parser] impl lexem processing via class methods

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -4,14 +4,48 @@
 #include <FlexLexer.h>
 #endif
 
+// need to forward parser types to lexer
+#include "grammar.tab.hh"
+
+// here all lexems should be processed
 class Lexer : public yyFlexLexer {
     std::string current_lexem{};
     std::string current_value{};
 
-    int process_plus() {
+    yy::parser::token_type process_plus() {
         current_lexem = "operator";
         current_value = "+";
-        return 1;
+        return yy::parser::token_type::PLUS;
+    }
+
+    yy::parser::token_type process_minus() {
+        current_lexem = "operator";
+        current_value = "-";
+        return yy::parser::token_type::MINUS;
+    }
+
+    yy::parser::token_type process_equal() {
+        current_lexem = "operator";
+        current_value = "*";
+        return yy::parser::token_type::EQUAL;
+    }
+
+    yy::parser::token_type process_scolon() {
+        current_lexem = "operator";
+        current_value = ";";
+        return yy::parser::token_type::SCOLON;
+    }
+
+    yy::parser::token_type process_number() {
+        current_lexem = "value";
+        current_value = "number";
+        return yy::parser::token_type::NUMBER;
+    }
+
+    yy::parser::token_type process_error() {
+        current_lexem = "debug";
+        current_value = "error";
+        return yy::parser::token_type::ERR;
     }
 
   public:
