2eb0dc4b1fa03f0b4fb39afc2573893967844674
BruceGliff <ilyaawaA@yandex.ru>
2022-12-06 16:54:42 -0500

    Type.h small refactoring

--- include/AST/Type.h
+++ include/AST/Type.h
@@ -1,31 +1,37 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <ranges>
 #include <vector>
 
 namespace ast {
+
+// Forward declaration.
+class TypeBuilder;
+// ExprNode is the placeholder to put here expresion node from AST.
+class ExprNode;
 
 class Hash {
   size_t V{0x42};
   bool IsSetted{false};
 
 public:
+  // Safe cast operator, which emits a Warning if smth is broken.
   operator size_t() const {
     if (!IsSetted)
       std::cerr << "Not setted hash.\n";
     return V;
   }
   Hash() = default;
-  Hash(size_t VIn) : V{VIn}, IsSetted{true} {}
+  Hash(size_t VIn) { set(VIn); }
   void set(size_t VIn) {
     V = VIn;
     IsSetted = true;
   }
   bool isSetted() const { return IsSetted; }
 };
-
-std::ostream &operator<<(std::ostream &os, Hash const &H) {
+inline std::ostream &operator<<(std::ostream &os, Hash const &H) {
   if (H.isSetted())
     os << std::hex << (size_t)H << std::dec;
   else
@@ -33,94 +39,112 @@ std::ostream &operator<<(std::ostream &os, Hash const &H) {
   return os;
 }
 
-struct Type {
+// Base class describes basic info about all Types.
+// It contains hash info.
+// Unique type has unique pointer to it, therefore, hash conStructTys from this.
+class TypeBase {
   Hash HValue;
 
-  Type() { HValue.set(reinterpret_cast<size_t>(this)); }
+protected:
+  TypeBase() { HValue.set(reinterpret_cast<size_t>(this)); }
 
-  size_t getHash() const { return HValue; };
-  void setHash(size_t Hash) { HValue.set(Hash); }
-
-  virtual void print(std::ostream &os) const {
-    os << Dsc << '[' << std::hex << HValue << ']';
-  }
-
-private:
-  static constexpr char Dsc[] = "Type";
+public:
+  Hash const &getHash() const { return HValue; };
+  virtual void print(std::ostream &os) const = 0;
+  virtual ~TypeBase(){};
 };
 
+// out operator to print all Types.
 template <typename T>
-  requires std::is_base_of_v<Type, T>
+  requires std::is_base_of_v<TypeBase, T>
 inline std::ostream &operator<<(std::ostream &os, T const &Ty) {
   Ty.print(os);
   return os;
 }
 
-struct Int : Type {
+class IntTy : public TypeBase {
+  // TODO get rid of friend?
+  friend class TypeBuilder;
   size_t const Size{32};
-  Int() = default;
-  Int(std::unsigned_integral auto Size) : Size{static_cast<size_t>(Size)} {}
+  ExprNode *PLACEHOLDER{};
 
+protected:
+  IntTy() = default;
+  IntTy(std::unsigned_integral auto Size) : Size{static_cast<size_t>(Size)} {}
+
+public:
   virtual void print(std::ostream &os) const override {
-    os << Dsc << '[' << HValue << "]<Size=" << Size << '>';
+    os << Dsc << '[' << getHash() << "]<Size=" << Size << '>';
   }
+  virtual ~IntTy() {}
 
 private:
-  static constexpr char Dsc[] = "Int";
+  static constexpr char Dsc[] = "IntTy";
 };
 
-struct ConstInt : Int {
+class ConstIntTy final : public IntTy {
+  friend class TypeBuilder;
   int const Value{0};
 
-  ConstInt(std::integral auto Value) : Value{Value} {}
+  ConstIntTy(std::integral auto Value) : Value{Value} {}
 
+public:
   void print(std::ostream &os) const override {
-    os << Dsc << '[' << HValue << "]<Value=" << Value << '>';
+    os << Dsc << '[' << getHash() << "]<Value=" << Value << '>';
   }
 
 private:
-  static constexpr char Dsc[] = "ConstInt";
+  static constexpr char Dsc[] = "ConstIntTy";
 };
 
-struct Float : Type {
+class FloatTy final : public TypeBase {
+  friend class TypeBuilder;
+  ExprNode *PLACEHOLDER{};
 
+public:
   void print(std::ostream &os) const override {
-    os << Dsc << '[' << HValue << ']';
+    os << Dsc << '[' << getHash() << ']';
   }
 
 private:
-  static constexpr char Dsc[] = "Float";
+  static constexpr char Dsc[] = "FloatTy";
 };
 
-struct Double : Type {
+class DoubleTy final : public TypeBase {
+  friend class TypeBuilder;
+  ExprNode *PLACEHOLDER{};
 
+public:
   void print(std::ostream &os) const override {
-    os << Dsc << '[' << HValue << ']';
+    os << Dsc << '[' << getHash() << ']';
   }
 
 private:
-  static constexpr char Dsc[] = "Double";
+  static constexpr char Dsc[] = "DoubleTy";
 };
 
-struct Array : Type {
-  Int *Size;
-  Type *Base;
+class ArrayTy final : public TypeBase {
+  friend class TypeBuilder;
+  IntTy *Size;
+  TypeBase *Base;
 
-  Array(Int *Size, Type *Base) : Size{Size}, Base{Base} {}
+  ArrayTy(IntTy *Size, TypeBase *Base) : Size{Size}, Base{Base} {}
 
+public:
   void print(std::ostream &os) const override {
-    os << Dsc << '[' << HValue << "]<Size=" << *Size << ", Base=" << *Base
+    os << Dsc << '[' << getHash() << "]<Size=" << *Size << ", Base=" << *Base
        << '>';
   }
 
 private:
-  static constexpr char Dsc[] = "Array";
+  static constexpr char Dsc[] = "ArrayTy";
 };
 
-struct Struct : Type {
-  std::vector<Type *> Elems;
+class StructTy final : public TypeBase {
+  friend class TypeBuilder;
+  std::vector<TypeBase *> Elems;
 
-  Struct(Type *One) : Elems{One} {}
+  StructTy(TypeBase *One) : Elems{One} {}
 
   template <typename T> void push_by1(T &&First) { Elems.push_back(First); }
 
@@ -130,20 +154,23 @@ struct Struct : Type {
     push_by1(std::forward<Args>(Other)...);
   }
 
-  template <typename... Args> Struct(Args &&...args) {
+  template <typename... Args> StructTy(Args &&...args) {
     // TODO reserve memory.
     push_by1(std::forward<Args>(args)...);
   }
 
+  // StructTy(std::ranges::view auto &v) Elems{v} : {}
+
+public:
   void print(std::ostream &os) const override {
-    os << Dsc << '[' << HValue << "]<Consists of: ";
+    os << Dsc << '[' << getHash() << "]<Consists of: ";
     for (auto *x : Elems)
       os << *x << "; ";
     os << '>';
   }
 
 private:
-  static constexpr char Dsc[] = "Struct";
+  static constexpr char Dsc[] = "StructTy";
 };
 
 } // namespace ast
--- test/lib/AST/token.cc
+++ test/lib/AST/token.cc
@@ -9,26 +9,26 @@ void ast::call() {
 
   ast::TypeBuilder TB;
 
-  auto *Int = TB.get<ast::Int>();
+  auto *Int = TB.get<ast::IntTy>();
   std::cout << *Int << '\n';
-  auto Int16 = TB.get<ast::Int>(16u);
+  auto Int16 = TB.get<ast::IntTy>(16u);
   assert(Int != Int16);
   std::cout << *Int16 << '\n';
 
-  auto Int16_2 = TB.get<ast::Int>(16u);
+  auto Int16_2 = TB.get<ast::IntTy>(16u);
   assert(Int16_2 == Int16);
   std::cout << *Int16_2 << '\n';
 
-  auto CI = TB.get<ast::ConstInt>(5);
+  auto CI = TB.get<ast::ConstIntTy>(5);
   std::cout << '\n' << *CI << '\n';
-  auto array = TB.get<ast::Array>(CI, Int);
+  auto array = TB.get<ast::ArrayTy>(CI, Int);
   std::cout << *array << '\n';
 
-  auto VA = TB.get<ast::Array>(Int16, Int);
+  auto VA = TB.get<ast::ArrayTy>(Int16, Int);
   assert(array != VA);
   std::cout << *VA << '\n';
 
   // Is it range, or init list?
-  auto S = TB.get<ast::Struct>(VA, array, CI, Int16, Int);
+  auto S = TB.get<ast::StructTy>(VA, array, CI, Int16, Int);
   std::cout << *S << '\n';
 }
